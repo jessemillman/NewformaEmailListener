22dc40a533077a67c7ececdc5f9809d00b043d76
jesse millman <jesse.millman@wge.com.au>
2015-10-30 11:54:36 +0800

    initial - publish to github

---
+++ DetectSavedNewforma/DetectSavedNewforma.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Net.Mail;
+
+
+namespace DetectSavedNewforma
+{
+	public partial class NewformaAddIn
+    {		
+		Outlook.Items mailItems;
+		const string PR_MAIL_HEADER_TAG = @"http://schemas.microsoft.com/mapi/proptag/0x007D001E";
+		public string Domain { get; set; }
+		public string EmailAddress { get; set; }
+
+		private void AddInStartUp(object sender, System.EventArgs e)
+		{
+			EmailAddress = this.Application.ActiveExplorer().Session.CurrentUser.AddressEntry.GetExchangeUser().PrimarySmtpAddress;
+			MailAddress address = new MailAddress(EmailAddress);
+			Domain = address.Host;
+
+			Outlook.MAPIFolder inbox = Application.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+			mailItems = inbox.Items;
+
+			mailItems.ItemAdd += new Outlook.ItemsEvents_ItemAddEventHandler(ThreadStarter);
+		}
+
+		#region VSTO generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(AddInStartUp);
+		}
+		#endregion
+
+		private void ThreadStarter(Object Item)
+		{
+			System.Threading.Thread incomingMailThread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(this.InboxFolderItemAdded));
+			incomingMailThread.IsBackground = true;
+			incomingMailThread.Start(Item);
+		}
+
+		private void InboxFolderItemAdded(object Item)
+		{
+			ProcessMailItem(Item);
+		}
+
+		private void ProcessMailItem(object Item)
+		{
+			if (Item is Outlook.MailItem)
+			{
+				Outlook.MailItem mailItem = (Item as Outlook.MailItem);
+				Outlook.PropertyAccessor propertyAccessor = mailItem.PropertyAccessor as Outlook.PropertyAccessor;
+
+				if (mailItem.SenderEmailAddress.Contains(Domain))
+				{
+					try
+					{
+						string headers = (string)propertyAccessor.GetProperty(PR_MAIL_HEADER_TAG);
+
+						if (headers.Contains("x-newforma-client-submit-time"))
+						{
+							string existingCategories = mailItem.Categories;
+							if (String.IsNullOrEmpty(existingCategories))
+							{
+								mailItem.Categories = "Filed By Newforma";
+							}
+							else
+							{
+								if (mailItem.Categories.Contains("Filed By Newforma") == false)
+								{
+									mailItem.Categories = existingCategories + ", Filed By Newforma";
+								}
+							}
+							mailItem.Save();
+						}
+					}
+					catch { }
+				}
+			}
+		}
+	}
+}
---
+++ DetectSavedNewforma/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Newforma Email Listener")]
+[assembly: AssemblyDescription("Listens for internal email that has been saved by newforma and assigns the correct category.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jesse Millman")]
+[assembly: AssemblyProduct("Newforma Email Listener")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59b2821e-9f62-4c29-b07c-9f205c5ff2da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
